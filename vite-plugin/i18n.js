2b11c9c27617dfcfaff040a13c8a90e1cde76d6c
unknown <416212316@qq.com>
2021-11-28 00:06:47 +0800

    fix

--- vite-plugin/i18n.js
+++ vite-plugin/i18n.js
@@ -1,14 +1,14 @@
 export default {
-    // 将load进来的代码块进一步加工处理
-    transform(code, id) {
-        // 将i18n信息写入组件配置
-        // code是快的内容
-        // id请求的url
-        if(!/vue&type=i18n/.test(id)) {
-            return `export default Comp => {
+  // 将load进来的代码块进一步加工处理
+  transform(code, id) {
+    // 将i18n信息写入组件配置
+    // code是快的内容
+    // id请求的url
+    if (/vue&type=i18n/.test(id)) {
+      return `export default Comp => {
                 Comp.i18n = ${code}
-            }`
-        }
-        return null
+            }`;
     }
-}
+    return null;
+  },
+};
